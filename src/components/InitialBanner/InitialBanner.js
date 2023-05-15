7338ff8459717366b98d6c938dd168d5b77bf65e
nathanzd <nathan.fe.dias@gmail.com>
2023-05-14 21:16:38 -0300

    redeploy

--- src/components/InitialBanner/InitialBanner.js
+++ src/components/InitialBanner/InitialBanner.js
@@ -1,11 +1,12 @@
 import React from 'react'
-import banner from '../../img/InitialBanner5.jpg'
 import bannerMobile from '../../img/InitialBanner4Mobile1.jpg'
+// import banner from '../../img/InitialBanner5.jpg'
+import Banner from '../../img/Initial.jpg'
 import styles from './InitialBanner.module.css'
 const InitialBanner = () => {
   return (
     <div className={styles.initialBanner}>
-        <img className={styles.initialBannerImage} src={banner.src} alt='banner'/>
+        <img className={styles.initialBannerImage} src={Banner.src} alt='banner'/>
         <img className={styles.initialBannerImageMobile} src={bannerMobile.src} alt='banner'/>
 
     </div>
