9901363ab9fe22bc64417fe2c791e66da8daab40
nathanzd <nathan.fe.dias@gmail.com>
2025-03-19 12:53:40 -0300

    ts

--- src/components/CustomBanner/CustomBanner.js
+++ src/components/CustomBanner/CustomBanner.js
@@ -4,7 +4,7 @@ import styles from '../InitialBanner/InitialBanner.module.css'
 const CustomBanner = ({mobile,desktop}) => {
     return (
         <div className={styles.initialBanner}>
-            <img className={styles.initialBannerImage} src={desktop.src} alt='banner'/>
+            <img className={styles.initialBannerImage} src={desktop.src} alt='banner2'/>
             <img className={styles.initialBannerImageMobile} src={mobile.src} alt='banner'/>
     
         </div>
